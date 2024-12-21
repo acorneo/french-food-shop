4c34a061c3bbb6a7efb3bf77556ec41785db3815
Maksim Raklo <mraklo@bk.ru>
2024-12-21 20:41:22 +0300

    add styles and layout for navigation and app components

--- frontend/src/screens/App.tsx
+++ frontend/src/screens/App.tsx
@@ -1,7 +1,16 @@
 import Navigation from "./components/Navigation";
+import styles from "./App.module.scss";
 
 const App = () => {
-  return <Navigation />;
+  return (
+    <>
+      <div className={styles["content-wrapper"]}>
+        <div className={styles["nothing"]} />
+        <Navigation />
+        <div className={styles["nothing"]} />
+      </div>
+    </>
+  );
 };
 
 export default App;
--- frontend/src/screens/components/Navigation.tsx
+++ frontend/src/screens/components/Navigation.tsx
@@ -6,6 +6,7 @@ import {
   OtherGoodsLink,
   PastriesLink,
 } from "../../constants";
+import styles from "./Navigation.module.scss";
 
 interface NavigationProps {
   title: string;
@@ -14,7 +15,7 @@ interface NavigationProps {
 
 const NavigationLink: React.FC<NavigationProps> = ({ title, link }) => {
   return (
-    <p className="nav-link">
+    <p className={styles["nav-link"]}>
       <a href={link}>{title}</a>
     </p>
   );
@@ -22,11 +23,16 @@ const NavigationLink: React.FC<NavigationProps> = ({ title, link }) => {
 
 const Navigation = () => {
   return (
-    <div className="nav-container">
-      <div className="nav-logo">
+    <div className={styles["nav-container"]}>
+      <div className={styles["nav-logo"]}>
         <img src={LogoImage} alt="Logo" />
+        <h4>
+          Croissant
+          <br />
+          World
+        </h4>
       </div>
-      <div className="nav-links">
+      <div className={styles["nav-links"]}>
         <NavigationLink title="Novelties" link={NoveltiesLink} />
         <NavigationLink title="Pastries" link={PastriesLink} />
         <NavigationLink title="Combos" link={CombosLink} />
