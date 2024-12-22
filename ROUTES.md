72777a9572b9ff37253cf99ec9a6b66cef6ed6b7
Maksim <you@example.com>
2024-12-22 22:50:37 +0300

    IT FUCKING WORKS

---
+++ ROUTES.md
@@ -0,0 +1,22 @@
+## Routes are listed here so I don't forget them
+documentation, but better.
+
+### Ping
+- GET /api/ping
+
+### Food Images
+- GET /api/images/croissant
+- GET /api/images/coca_cola
+- GET /api/images/baguette
+- GET /api/images/lemonade
+- GET /api/images/macarons
+- GET /api/images/mojito
+- GET /api/images/tartiflette
+
+### Often Ordered
+- GET /api/often-order
+
+### Categories
+- GET /api/categories/novelties
+- GET /api/categories/pastries
+- GET /api/categories/lunch
--- frontend/src/screens/pages/Home.tsx
+++ frontend/src/screens/pages/Home.tsx
@@ -1,17 +1,100 @@
-import LemonadeImage from "../../assets/lemonade.png";
 import styles from "./Home.module.scss";
 import SmallCard from "../components/SmallCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Product {
+  image_url: string;
+  title: string;
+  price: number;
+  product_id: number;
+}
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [oftenOrderData, setOftenOrderData] = useState<Product[]>([]);
+  const [noveltiesData, setNoveltiesData] = useState<Product[]>([]);
+  const [pastriesData, setPastriesData] = useState<Product[]>([]);
+  const [lunchData, setLunchData] = useState<Product[]>([]);
+
+  useEffect(() => {
+    setLoading(true);
+    try {
+      axios.get("/api/often-order").then((r) => {
+        setOftenOrderData(r.data.products);
+      });
+      axios.get("api/categories/novelties").then((r) => {
+        setNoveltiesData(r.data.products);
+      });
+      axios.get("api/categories/pastries").then((r) => {
+        setPastriesData(r.data.products);
+      });
+      axios.get("api/categories/lunch").then((r) => {
+        setLunchData(r.data.products);
+      });
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return (
     <>
       <h2 className={styles["oft-order-text"]}>Often Order</h2>
-      <SmallCard
-        image_url={LemonadeImage}
-        title="Lemonade"
-        price={1.99}
-        product_id={1000 - 7}
-      />
+      {loading ? (
+        <h3>Loading products...</h3>
+      ) : (
+        oftenOrderData.map((item) => (
+          <SmallCard
+            image_url={item.image_url}
+            title={item.title}
+            price={item.price}
+            product_id={item.product_id}
+          />
+        ))
+      )}
+
+      <h1 className={styles["category-text"]}>Novelties</h1>
+      {loading ? (
+        <h3>Loading products...</h3>
+      ) : (
+        noveltiesData.map((item) => (
+          <SmallCard
+            image_url={item.image_url}
+            title={item.title}
+            price={item.price}
+            product_id={item.product_id}
+          />
+        ))
+      )}
+
+      <h1 className={styles["category-text"]}>Pastires</h1>
+      {loading ? (
+        <h3>Loading products...</h3>
+      ) : (
+        pastriesData.map((item) => (
+          <SmallCard
+            image_url={item.image_url}
+            title={item.title}
+            price={item.price}
+            product_id={item.product_id}
+          />
+        ))
+      )}
+
+      <h1 className={styles["category-text"]}>Lunch</h1>
+      {loading ? (
+        <h3>Loading products...</h3>
+      ) : (
+        lunchData.map((item) => (
+          <SmallCard
+            image_url={item.image_url}
+            title={item.title}
+            price={item.price}
+            product_id={item.product_id}
+          />
+        ))
+      )}
     </>
   );
 };
